2ce7cab129e80b59a0386d63fd35bbd276e690dd
TheLazieR Yip <thelazier@gmail.com>
2016-05-19 13:31:31 +0700

    v0.12.1.x update openssl 1.0.1t

--- v0.12.1.x/build.sh
+++ v0.12.1.x/build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # build
-apk --update add build-base git tar perl autoconf automake libtool linux-headers patch
+apk --update add build-base git tar perl autoconf automake libtool linux-headers patch file
 
 # Clone
 git clone -b v0.12.1.x --depth 1 https://github.com/dashpay/dash.git
 
 # Compile & Install
 cd dash
-# Patch for OpenSSL 1.0.1r (from 1.0.1k)
-curl "https://patch-diff.githubusercontent.com/raw/dashpay/dash/pull/699.patch" |patch -p1
+# Patch for OpenSSL 1.0.1t (from 1.0.1k)
+curl -s https://raw.githubusercontent.com/thelazier/docker-dashd/master/v0.12.1.x/openssl1.0.1t.patch | patch -p1
 cd depends
 make NO_QT=1 HOST=x86_64-pc-linux-gnu
 cd ..
@@ -18,11 +18,12 @@ cd ..
 make
 make test
 make install
-cp -rv `pwd`/depends/x86_64-pc-linux-gnu/. /usr/local/.
+cp -rv `pwd`/depends/x86_64-pc-linux-gnu/bin /usr/local/bin
+cp -rv `pwd`/depends/x86_64-pc-linux-gnu/lib /usr/local/lib
 
 # Cleanup
 cd /
-apk del build-base git tar perl autoconf automake libtool linux-headers patch
+apk del build-base git tar perl autoconf automake libtool linux-headers patch file
 rm -r /var/cache/apk/*
 rm -r /dash
 
