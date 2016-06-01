21bf1b0e7f599b78bd0540555bb5087f8884d8eb
TheLazieR Yip <thelazier@gmail.com>
2016-06-01 20:16:10 +0700

    Fix Typo

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ git clone -b v0.12.1.x --depth 1 https://github.com/dashpay/dash.git
 # Compile & Install
 cd dash
 # Patch for OpenSSL 1.0.1t (from 1.0.1k)
-curl -s https://raw.githubusercontent.com/thelazier/docker-dashd/master/v0.12.1.x/patches/openssl1.0.1t.patch | patch -p1
+curl -s https://raw.githubusercontent.com/thelazier/docker-dashd/v0.12.1.x/patches/openssl1.0.1t.patch | patch -p1
 cd depends
 make NO_QT=1 HOST=x86_64-pc-linux-gnu
 cd ..
